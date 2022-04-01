f742e48abf16c19966c1894dd04c6b902b2d3671
Lourdes14 <85315075+Lourdes14@users.noreply.github.com>
2022-04-01 08:37:06 -0600

    proyectoooLS

---
+++ Proyecto/Controller/Operaciones.Controller.php
@@ -0,0 +1,13 @@
+<?php
+class Operaciones{
+    public $smarty;
+    public function __construct(){
+        //$this->smarty=new Smarty();
+
+    }
+    public function Inicio(){
+        echo 'Controller Operaciones , Method Inicio';
+    }
+}
+
+?>
